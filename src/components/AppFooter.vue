1515f82b7de514a9073561fa91f7b2b2ef0dc160
snerva <selenenerva@live.it>
2022-11-21 20:01:29 +0100

    area footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,10 +7,25 @@ export default {
 
 <template>
     <footer>
-
+        <div class="container">
+            <div class="row row-cols-4 g-3 py-4">
+                <div class="col p-3">
+                    <h6>About</h6>
+                </div>
+                <div class="col p-3">
+                    <h6>Contact</h6>
+                </div>
+                <div class="col p-3">
+                    <h6>Pages</h6>
+                </div>
+                <div class="col p-3">
+                    <h6>Blog</h6>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
