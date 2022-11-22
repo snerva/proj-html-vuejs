a4ed295904ff70f85af27c4cf447624da6da6921
snerva <selenenerva@live.it>
2022-11-22 18:19:16 +0100

    courses and banner read_more

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -28,72 +28,94 @@ export default {
         <section class="courses">
             <div class="container">
                 <div class="cards">
-                    <div class="row row-cols-3 g-3">
-                        <div class="col">col 1</div>
-                        <div class="col">col 2</div>
-                        <div class="col">col 3</div>
+                    <div class="row row-cols-3">
+                        <div class="col px-3">
+                            <div class="single_card">
+                                <font-awesome-icon icon="fa-solid fa-earth-africa" class="icon " />
+                                <h6>Worldwide Recognize</h6>
+                                <p>Our online course certificates can be used around the world and also in most popular
+                                    universities and companies.</p>
+                            </div>
+                        </div>
+                        <div class="col px-3">
+                            <div class="single_card">
+                                <font-awesome-icon icon="fa-solid fa-desktop" class="icon" />
+                                <h6>Mostly Online Learning (sd)</h6>
+                                <p>Masterstudy online certificates can be obtained in a range of specialized areas and
+                                    typically take about a year to complete.</p>
+                            </div>
+
+                        </div>
+                        <div class="col px-3">
+                            <div class="single_card">
+                                <font-awesome-icon icon="fa-regular fa-calendar-check" class="icon" />
+                                <h6>Graduate in as little as 1 Year</h6>
+                                <p>Online post graduate certificates are a popular way to develop your professional
+                                    qualifications of Masterstudy</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="courses_groups">
                     <div class="row row-cols-2 g-3">
                         <div class="col-8 pt-2">
-                            <div class="image bg-success">
-                                <img src="" alt="">
+                            <div class="image">
+                                <img src="../../public/images/cat_1-770x375.jpg" alt="">
                             </div>
-                            <div class="text">
-                                <span>lorem</span>
-                                <a class="float-end" href="">Courses</a>
+                            <div class="text d-flex justify-content-between align-items-center mt-3">
+                                <h6>Software Development</h6>
+                                <a class="float-end" href="">5 Courses</a>
                             </div>
                         </div>
-                        <div class="col-4 py-1 px-2">
+                        <div class="col-4 pt-2 px-2">
                             <div class="row row-cols-1 g-3">
-                                <div class="col p-1">
-                                    <div class="image bg-success">
-                                        <img src="" alt="">
+                                <div class="col ps-3">
+                                    <div class="image">
+                                        <img src="../../public/images/cat_2-370x155.jpg" alt="">
                                     </div>
-                                    <div class="text">
-                                        <span>lorem</span>
-                                        <a class="float-end" href="">Courses</a>
+                                    <div class="text d-flex justify-content-between align-items-center mt-3">
+                                        <h6>Art</h6>
+                                        <a class="float-end" href="">8 Courses</a>
                                     </div>
                                 </div>
-                                <div class="col p-1">
-                                    <div class="image bg-success">
-                                        <img src="" alt="">
+                                <div class="col ps-3 pt-2">
+                                    <div class="image">
+                                        <img src="../../public/images/cat_3-370x155.jpg" alt="">
                                     </div>
-                                    <div class="text">
-                                        <span>lorem</span>
-                                        <a class="float-end" href="">Courses</a>
+                                    <div class="text d-flex justify-content-between align-items-center mt-3">
+                                        <h6>Material Design</h6>
+                                        <a class="float-end" href="">5 Courses</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="row row-cols-3 g-3">
-                        <div class="col p-2">
-                            <div class="image bg-success">
-                                <img src="" alt="">
+                        <div class="col py-4">
+                            <div class="image">
+                                <img src="../../public/images/cat_5-370x155.jpg" alt="">
                             </div>
-                            <div class="text">
-                                <span>lorem</span>
-                                <a class="float-end" href="">Courses</a>
-                            </div>
-                        </div>
-                        <div class="col p-2">
-                            <div class="image bg-success">
-                                <img src="" alt="">
-                            </div>
-                            <div class="text">
-                                <span>lorem</span>
-                                <a class="float-end" href="">Courses</a>
+                            <div class="text d-flex justify-content-between align-items-center mt-3">
+                                <h6>Music</h6>
+                                <a class="float-end" href="">5 Courses</a>
                             </div>
                         </div>
-                        <div class="col p-2">
-                            <div class="image bg-success">
-                                <img src="" alt="">
+                        <div class="col py-4">
+                            <div class="image">
+                                <img src="../../public/images/cat_4-370x155.jpg" alt="">
                             </div>
-                            <div class="text">
-                                <span>lorem</span>
-                                <a class="float-end" href="">Courses</a>
+                            <div class="text d-flex justify-content-between align-items-center mt-3">
+                                <h6>Exercise</h6>
+                                <a class="float-end" href="">7 Courses</a>
+                            </div>
+                        </div>
+                        <div class="col py-4">
+                            <div class="image">
+                                <img src="../../public/images/cat_6-370x155.jpg" alt="">
+                            </div>
+                            <div class="text d-flex justify-content-between align-items-center mt-3">
+                                <h6>Photography</h6>
+                                <a class="float-end" href="">4 Courses</a>
                             </div>
                         </div>
                     </div>
@@ -102,7 +124,17 @@ export default {
             </div>
         </section>
         <!--/.courses-->
-        <section class="banner read_more"></section>
+        <section class="banner read_more d-flex align-items-center">
+            <div class="container">
+                <div class="contents">
+                    <h2>Limitless learning, more possibilities</h2>
+                    <p>Online courses open the opportunity for learning to almost anyone, regardless of their scheduling
+                        commitments.
+                    </p>
+                    <a class="btn rounded-pill" href="" role="button">JOIN READ MORE</a>
+                </div>
+            </div>
+        </section>
         <!--/.read_more-->
         <section class="recents text-center">
             <h2>Recent courses</h2>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faTwitter, faInstagram, faBehance, faDribbble, faFlickr, faGit, faLinkedin, faPinterest } from '@fortawesome/free-brands-svg-icons'
-import { faBars, faMagnifyingGlass, faBullhorn, faBriefcase } from '@fortawesome/free-solid-svg-icons'
-import { faUser, faBookmark } from '@fortawesome/free-regular-svg-icons'
+import { faBars, faMagnifyingGlass, faBullhorn, faBriefcase, faDesktop, faEarthAfrica } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faBookmark, faCalendarCheck } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faTwitter, faInstagram, faBehance, faDribbble, faFlickr, faGit, faLinkedin, faPinterest, faBars, faMagnifyingGlass, faBullhorn, faBriefcase, faUser, faBookmark)
+library.add(faTwitter, faInstagram, faBehance, faDribbble, faFlickr, faGit, faLinkedin, faPinterest, faBars, faMagnifyingGlass, faBullhorn, faBriefcase, faUser, faBookmark, faCalendarCheck, faEarthAfrica, faDesktop)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
