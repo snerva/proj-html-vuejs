faeb3d8f416f699e14b67519780c0ce32d598f44
snerva <selenenerva@live.it>
2022-11-22 13:43:14 +0100

    main initial commit and start banner

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,7 +7,23 @@ export default {
 
 <template>
     <main>
-        <section class="banner start"></section>
+        <section class="banner start">
+            <div class="container  d-flex align-items-center">
+                <div class="contents">
+                    <h1>Start Investing in You</h1>
+                    <p>With over 1200 courses in 18 subjects, you're guaranteed to find something that's right for you.
+                    </p>
+                    <a class="btn rounded-pill" href="" role="button">JOIN FOR FREE</a>
+                </div>
+                <div class="start_images position-relative">
+                    <img class="bubblespeech position-absolute" src="../../public/images/bubblespeech.png" alt="">
+                    <img class="book1 position-absolute" src="../../public/images/book1.png" alt="">
+                    <img class="book2 position-absolute" src="../../public/images/book2.png" alt="">
+                    <img class="magnifier position-absolute" src="../../public/images/magnifier.png" alt="">
+                    <img class="base" src="../../public/images/base.png" alt="">
+                </div>
+            </div>
+        </section>
         <!--/.banner-->
         <section class="courses">
             <div class="container">
