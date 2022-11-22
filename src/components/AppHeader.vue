7abf6d5f7afcbd20d6943ea92d0e865e25c9441f
snerva <selenenerva@live.it>
2022-11-22 10:27:06 +0100

    navbar languages and links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,9 +7,44 @@ export default {
 
 <template>
     <header>
-        <div class="header_top d-flex justify-content-between align-items-center">
+        <div class="header_top d-flex justify-content-between align-items-center py-2">
+            <div class="language_form">
+                <select name="languages" id="languages" class="border border-0 p-2">
+                    <option value="english">English</option>
+                </select>
+            </div>
+            <div class="navbar_menu">
+                <div class="nav_links d-flex align-items-center">
+                    <div class="courses">
+                        <a href="">Courses</a>
+                    </div>
+                    <div class="zoom position-relative">
+                        <small class="new px-1">NEW</small>
+                        <a href="">Zoom</a>
+                    </div>
+                    <div class="pages">
+                        <a href="">Pages</a>
+                    </div>
+                    <div class="bundles">
+                        <a href="">Bundles</a>
+                    </div>
+                    <div class="format">
+                        <a href="">Course Formats</a>
+                    </div>
+                    <div class="add position-relative">
+                        <small class="new px-1">NEW</small>
+                        <a href="">Add Courses</a>
+                    </div>
+                    <div class="demos">
+                        <a href="">Demos</a>
+                    </div>
+                </div>
+                <!--/.navbar-links-->
+                <div class="socials">
+                </div>
+            </div>
         </div>
-        <hr>
+        <hr class="m-0">
         <div class="header_bottom d-flex">
 
         </div>
