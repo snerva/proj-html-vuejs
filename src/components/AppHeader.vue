2d4b005c77b856f959ffb14797eaa3b5532027bf
snerva <selenenerva@live.it>
2022-11-22 12:25:04 +0100

    header bottom and button hover effect

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,6 +13,7 @@ export default {
                     <option value="english">English</option>
                 </select>
             </div>
+            <!--/.language_form-->
             <div class="navbar_menu d-flex">
                 <div class="nav_links d-flex align-items-center px-3">
                     <div class="courses">
@@ -69,9 +70,58 @@ export default {
                 <!--/.socials-->
             </div>
         </div>
+        <!--/.header_top-->
         <hr class="m-0">
-        <div class="header_bottom d-flex">
-
+        <div class="header_bottom d-flex justify-content-between align-items-center py-4">
+            <div class="logo">
+                <img src="../../public/images/MasterStudy_logo.svg" alt="">
+            </div>
+            <!--/.logo-->
+            <div class="search_courses d-flex">
+                <div class="category_list d-flex align-items-center pe-2">
+                    <font-awesome-icon icon="fa-solid fa-bars" size="lg" />
+                    <span class="px-1">CATEGORY</span>
+                </div>
+                <div class="searchbar input-group">
+                    <input type="text" class="form-control" placeholder="Search courses" aria-label="Search courses"
+                        aria-describedby="button-addon2">
+                    <button class="btn px-3" type="button" id="button-addon2">
+                        <font-awesome-icon icon="fa-solid fa-magnifying-glass" size="md" />
+                    </button>
+                </div>
+            </div>
+            <!--/.search_courses-->
+            <div class="typologies d-flex">
+                <div class="instructor px-2">
+                    <a href="">
+                        <font-awesome-icon icon="fa-solid fa-bullhorn" />
+                    </a>
+                    <span class="px-2">Become an Instructor</span>
+                </div>
+                <div class="enterprise px-2">
+                    <a href="">
+                        <font-awesome-icon icon="fa-solid fa-briefcase" />
+                    </a>
+                    <span class="px-2">For Enterprise</span>
+                </div>
+            </div>
+            <!--/.typologies-->
+            <div class="account d-flex align-items-center">
+                <div class="login">
+                    <a href="">
+                        <font-awesome-icon icon="fa-regular fa-user" />
+                        <span class="px-2">Log In</span>
+                    </a>
+                </div>
+                <div class="sign_up">
+                    <a class="btn rounded-pill" href="" role="button">SIGN UP</a>
+                </div>
+                <div class="bookmark">
+                    <a href="">
+                        <font-awesome-icon icon="fa-regular fa-bookmark" size="2xl" />
+                    </a>
+                </div>
+            </div>
         </div>
     </header>
 </template>
