1c481702d3082e01f8aca573acf81afc489639c2
snerva <selenenerva@live.it>
2022-11-23 09:59:57 +0100

    data banner

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -407,7 +407,40 @@ export default {
             </div>
         </section>
         <!--/.recents-->
-        <section class="banner data bg-secondary"></section>
+        <section class="banner data">
+            <div class="container text-center">
+                <div class="title">
+                    <h2>We are Proud</h2>
+                    <p class="mt-3">Certificate courses bare instructed by highly educated and qualified instructors who
+                        hold
+                        doctoral
+                        and
+                        master`s level degrees.</p>
+                </div>
+                <div class="row row-cols-4">
+                    <div class="col border-end">
+                        <span class="data_numbers">2000</span>
+                        <br>
+                        <span>STUDENTS</span>
+                    </div>
+                    <div class="col border-end">
+                        <span class="data_numbers">950</span>
+                        <br>
+                        <span>COURSES</span>
+                    </div>
+                    <div class="col border-end">
+                        <span class="data_numbers">1600</span>
+                        <br>
+                        <span>HOURS VIDEO</span>
+                    </div>
+                    <div class="col border-end">
+                        <span class="data_numbers">150</span>
+                        <br>
+                        <span>COUNTRIES REACHED</span>
+                    </div>
+                </div>
+            </div>
+        </section>
         <!--/.data-->
         <section class="subscribe">
             <div class="container d-flex py-5">
