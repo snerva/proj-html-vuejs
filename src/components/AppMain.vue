8542a4b61aa0122ef8447f494e26f8ed6e31f7eb
snerva <selenenerva@live.it>
2022-11-23 12:19:52 +0100

    testimonials

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -639,8 +639,18 @@ export default {
             </div>
         </section>
         <!--/.categories-->
-        <section class="opinions">
-
+        <section class="testimonials d-flex align-items-center">
+            <div class="opinion">
+                <h4>Come as you are</h4>
+                <p>MasterStudy used is an excellent workshop wheter you come as a counselor, advisor, administrator, or
+                    faculty member. I am going home empowered. I am looking forward to attending the On Course
+                    MasterStudy and the MasterStudy National Conference next year and facilitating the use of this
+                    excellent retention/student success course at my college.</p>
+                <span><strong>Tim Sab</strong></span>
+                <br>
+                <span><strong><small>General Developer, Stylemix Themes</small></strong></span>
+            </div>
+            <div class="lateral_triangle"></div>
         </section>
         <!--/.opinions-->
     </main>
