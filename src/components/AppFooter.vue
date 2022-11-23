7c111f217d6187643730bdb685d2b7b9c54790eb
snerva <selenenerva@live.it>
2022-11-23 15:17:07 +0100

    footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,19 +7,67 @@ export default {
 
 <template>
     <footer>
-        <div class="container">
-            <div class="row row-cols-4 g-3 py-4">
-                <div class="col p-3">
-                    <h6>About</h6>
+        <div class="contents">
+            <div class="row row-cols-4 g-3">
+                <div class="col p-3 small_text">
+                    <h6 class="mb-4">About</h6>
+                    <p><a href="">Masterstudy</a> is Education WordPress theme featured by Learning Managment System
+                        (LMS) for online education. Developed by <a href="">Stylemix Themes</a>.</p>
+                </div>
+                <div class="col py-3 ps-3 pe-5 small_text">
+                    <h6 class="mb-4">Contact</h6>
+                    <p>USA, California 20, First Avenue, California</p>
+                    <span>Tel.: +1 212 458 300 32</span>
+                    <br>
+                    <span>Fax: +1 212 375 24 14</span>
+                    <br>
+                    <p class="light_text mt-3">info@masterstudy.com</p>
                 </div>
                 <div class="col p-3">
-                    <h6>Contact</h6>
+                    <h6 class="mb-4">Pages</h6>
+                    <div class="pages_list d-flex">
+                        <ul class="px-3">
+                            <li>Blog</li>
+                            <li>Home</li>
+                            <li>Shortcodes</li>
+                        </ul>
+                        <ul>
+                            <li>Courses</li>
+                            <li>Membership A...</li>
+                            <li>Typography</li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col p-3">
-                    <h6>Pages</h6>
-                </div>
-                <div class="col p-3">
-                    <h6>Blog</h6>
+                    <h6 class="mb-4">Blog</h6>
+                    <div class="post d-flex">
+                        <img class="post_img" src="../../public/images/photo-1517520287167-4bbf64a00d66-75x75.jpeg"
+                            alt="">
+                        <div class="text px-3">
+                            <figure>
+                                <blockquote class="blockquote">
+                                    <p>Our main target is to "Developing Yourself as a Leader"</p>
+                                </blockquote>
+                                <figcaption class="blockquote-footer">
+                                    August 9, 2018
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="post d-flex">
+                        <img class="img-fluid post_img"
+                            src="../../public/images/photo-1490376840453-5f616fbebe5b-75x75.jpeg" alt="">
+                        <div class="text px-3">
+                            <figure>
+                                <blockquote class="blockquote">
+                                    <p>Our main target is to "Developing Yourself as a Leader"</p>
+                                </blockquote>
+                                <figcaption class="blockquote-footer">
+                                    August 9, 2018
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
