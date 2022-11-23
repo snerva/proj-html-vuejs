78c961f4db0da21081c38dee7263c428e4576e76
snerva <selenenerva@live.it>
2022-11-23 10:21:53 +0100

    subscribe

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -445,13 +445,19 @@ export default {
         <section class="subscribe">
             <div class="container d-flex py-5">
                 <div class="newsletter">
-                    <h4>Lorem, ipsum dolor.</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque minus, exercitationem soluta
-                        dolores placeat unde dolor eius fugiat et, necessitatibus illum beatae? Totam, fugiat eum!</p>
+                    <h4>Subscribe our newsletter</h4>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                        alteration in some form, by injected humour, or randomised words.</p>
                 </div>
-                <div class="mail_form">
-                    <h4>Lorem, ipsum dolor.</h4>
-                    <input type="text" name="" id="">
+                <div class="mail_form px-5">
+                    <h4>YOUR E-MAIL ADDRESS</h4>
+                    <div class="searchbar input-group flex-nowrap">
+                        <input type="text" class="form-control" placeholder="Enter your E-mail"
+                            aria-label="Enter your E-mail" aria-describedby="button-addon2">
+                        <button class="btn px-3" type="button" id="button-addon2">
+                            SUBSCRIBE
+                        </button>
+                    </div>
                 </div>
             </div>
         </section>
